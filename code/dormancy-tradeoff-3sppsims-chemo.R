1307b12c0c302f4dc662b89284ed3128dc43e604
Andrew Letten <andrewletten@gmail.com>
2024-01-17 15:42:01 +1000

    add continuous resource supply simulation script

--- code/dormancy-tradeoff-3sppsims-chemo.R
+++ code/dormancy-tradeoff-3sppsims-chemo.R
@@ -1,18 +1,24 @@
 ####################################################################
-## dormancy-tradeoff-3sppsims.R: core analysis #####################
+## dormancy-tradeoff-3sppsims-chemo.R: continuous resource supply ##
 ## Letten, Yamamichi, Richardson and Ke ############################
 ## Microbial dormancy supports multi-species coexistence ###########
 ## under resource fluctuations #####################################
 ####################################################################
 
 # Core code for simulating three-way and two-way competition under 
-# different resource pulsing scenarios
+# continuous/chemostat resource supply
 
 ### Load required packages and project code
 library(deSolve)
 library(tidyverse)
 source("code/dormancy-tradeoff-functions.R")
 source("code/dormancy-tradeoff-spp-params.R")
+
+### Modify outflow/mortality parameters for chemostat dynamics ----------------
+parameters["d"] <-  0.025 # outflow/mortality rate for all species
+parameters["m"] <-  0 # 0 additional mortality for non-dormancy strategists
+parameters["ma"] <-  0 # 0 additional mortality for dormancy specialist (active phase)
+parameters["md"] <-  -0.0212 # reduced mortality for dormancy specialist (dormant phase)
 
 ### Change starting density of either N1_active, N2 or N3 to zero, to simulate pairwise dynamics 
 state <- c(N1_activ = 100,
@@ -21,26 +27,20 @@ state <- c(N1_activ = 100,
            N3 = 100,
            R = 10)
 
-### Setup resource parameter space to simulate across
-respulsesize <-  seq(5, 20, 1)
-respulsefreq <-  seq(24, 336, 24)
-respulsecombo <-  expand.grid(respulsesize, respulsefreq)
-names(respulsecombo) <- c("size", "freq")
+### Loop sims over different resource supply concentrations -------------------
+ressize <-  seq(5, 20, 1) # Resource concentrations to match pulsed dynamics
 
-### Loop sims over parameter space (patience required!)
 dat_list = list()
-for (i in 1:nrow(respulsecombo)){
-  pulsesize <- respulsecombo[i,1]
-  pulsefreq <- respulsecombo[i,2]
-  totaltime <- pulsefreq*ifelse((state[["N1_activ"]] > 0 & state[["N2"]] > 0 & state[["N3"]] > 0), 4000, 2000)
+for (i in 1:length(ressize)){
+  totaltime <- 2000
   times <- seq(0, totaltime, by = 1)
-  pulseseq <- round(seq(pulsefreq, totaltime, by = pulsefreq), 1)
   start_time <- Sys.time()
+  state["R"] = ressize[i]
+  parameters["R0"] = ressize[i]
   out <- ode(y = state, 
            times = times, 
            func = dorm_cr_ode, 
-           parms = parameters,
-           events = list(func = respulse, time = pulseseq))
+           parms = parameters)
   dat_iter <- out %>% data.frame() 
   dat_list[[i]] <-  dat_iter
   print(i)
@@ -48,15 +48,13 @@ for (i in 1:nrow(respulsecombo)){
   print(end_time - start_time)
 }
 
-### Save output (either as compressed csv files OR as a single .Rdata file)
+### Save output (either as compressed csv files OR as a single .Rdata file) ---
 #################################################
 ## Save individual csv.gz for each sim output
 # sppcombo <- "N1N2N3" # Change for pairwise combinations, e.g. "N1N2")
-# dir.create(file.path(paste0("data/", sppcombo, "_gz/"))) # same directory as for continuous resource supply sims
-# filenames <- pmap_vec(respulsecombo,
-#                       function(size, freq) {paste(sppcombo, paste0("p", size), paste0("i", freq), sep = "_")})
+# dir.create(file.path(paste0("data/", sppcombo, "_gz/"))) # same directory as for core sims (pulsed resource supply)
+# filenames <- paste(sppcombo, paste0("p", ressize), "i0", sep = "_")
 # names(dat_list) <- filenames
-# 
 # iwalk(dat_list, .progress = TRUE,
 #       function(x, y) {write_csv(x, file = paste0("data/", sppcombo, "_gz/", y, ".csv.gz"))})
 #################################################
@@ -65,3 +63,7 @@ for (i in 1:nrow(respulsecombo)){
 ## Save as single .Rdata file 
 # save(dat_list, file = "data/<FILENAME>.Rdata")
 #################################################
+
+
+
+
