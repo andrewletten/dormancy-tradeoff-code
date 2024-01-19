c920f664e5f4e9d639724dd3d93ec100bcdf5a88
Andrew Letten <andrewletten@gmail.com>
2024-01-19 15:20:58 +1000

    edit title to refer to correct file name

--- code/dormancy-tradeoff-fig3.R
+++ code/dormancy-tradeoff-fig3.R
@@ -1,5 +1,5 @@
 ####################################################################
-## dormancy-tradeoff-post-sim-analysis.R:  #########################
+## dormancy-tradeoff-fig3.R:  ######################################
 ## Letten, Yamamichi, Richardson and Ke ############################
 ## Microbial dormancy supports multi-species coexistence ###########
 ## under resource fluctuations #####################################
@@ -12,7 +12,7 @@ theme_set(theme_cowplot())
 source("code/dormancy-tradeoff-functions.R")
 
 ### Read individual csv files -------------------------------------------------
-sppcombo <- "N1N2" # Change for pairwise combinations, e.g. "N1N2")
+sppcombo <- "N1N2N3" # Change for pairwise combinations, e.g. "N1N2")
 dat_list <- extract_simfiles(sppcombo)
 
 ### Summarize coexistence/exclusion outcomes ----------------------------------
@@ -21,8 +21,8 @@ outcomes <- summarise_outcomes(dat_list)
 ### Make heat map of competitive outcomes -------------------------------------
 ## Uncomment`load(file = "data/3spp-coexistoutline.Rdata")` to load the outline 
 ## for the 3 species coexistence region and delete `= NULL` from call to `plot_comp_resource`.
-#load(file = "data/3spp-coexistoutline.Rdata") 
-simplot <- plot_comp_resource(outcomes, outline = NULL)
+load(file = "data/3spp-coexistoutline.Rdata") 
+simplot <- plot_comp_resource(outcomes, outline)
 simplot
 
 ############################
